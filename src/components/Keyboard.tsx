13a29c70ae42f84d32af9fd3685add708a3698bb
Anna Hippee <anna.hippee@gmail.com>
2024-03-22 09:57:10 -0400

    keyboard events working:

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,11 +1,41 @@
 import Key from "./Key";
+import { useContext } from "react";
+import { AppContext } from "../App";
+
+const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
 function Keyboard() {
-  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
-  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
-  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+  const { onEnter, onDelete, onSelectLetter } = useContext(AppContext);
+
+  const handleKeyboard = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Enter") {
+      onEnter();
+    } else if (event.key === "Backspace") {
+      onDelete();
+    } else {
+      const eventKey = event.key.toUpperCase();
+      keys1.forEach((key) => {
+        if (eventKey === key) {
+          onSelectLetter(key);
+        }
+      });
+      keys2.forEach((key) => {
+        if (eventKey === key) {
+          onSelectLetter(key);
+        }
+      });
+      keys3.forEach((key) => {
+        if (eventKey === key) {
+          onSelectLetter(key);
+        }
+      });
+    }
+  };
+
   return (
-    <div className="keyboard">
+    <div className="keyboard" onKeyDown={handleKeyboard} tabIndex={0}>
       <div className="line1">
         {keys1.map((key, i) => (
           <Key key={i} keyVal={key} />
